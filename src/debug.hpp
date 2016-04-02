a70831048ace02540c723b850aad178d76d3d3f5
Christian Neumüller <cn00@gmx.at>
2016-04-02 23:45:09 +0200

    debug: Default ind params to 0.

--- src/debug.hpp
+++ src/debug.hpp
@@ -9,8 +9,8 @@ namespace synth {
 void writeIndent(int ind);
 
 // If the locatio of the cursor is in f, no filename is printed.
-void dumpSingleCursor(CXCursor c, int ind, CXFile f = nullptr);
-void dumpAst(CXCursor c, int ind);
+void dumpSingleCursor(CXCursor c, int ind = 0, CXFile f = nullptr);
+void dumpAst(CXCursor c, int ind = 0);
 void writeLoc(std::ostream& out, CXSourceLocation loc, CXFile f = nullptr);
 void writeExtent(std::ostream& out, CXSourceRange rng, CXFile f = nullptr);
 std::ostream& operator<< (std::ostream& out, CXCursor c);
